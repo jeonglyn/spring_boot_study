37c520e95b96829c51df2e14e9a249fa98a058f1
jeonglyn <jeonglyn0622@jdinscorp.com>
2026-07-30 17:59:01 +0900

    Initial commit: Spring Boot 4.1.1 project setup

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.1.1-SNAPSHOT"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "spring_boot_study"
+version = "0.0.1-SNAPSHOT"
+description = "spring_boot_study"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testCompileOnly("org.projectlombok:lombok")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testAnnotationProcessor("org.projectlombok:lombok")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/java/spring_boot_study/spring_boot_study/SpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot_study.spring_boot_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
